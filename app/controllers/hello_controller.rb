2d401a68dd03f51925e50f5373633ae1e76b5d59
masashihamano <masashi.hamano@gmail.com>
2018-05-16 16:46:04 +0900

    chapter second fin

--- app/controllers/hello_controller.rb
+++ app/controllers/hello_controller.rb
@@ -4,4 +4,16 @@ class HelloController < ApplicationController
     render plain: "こんにちは、世界！"
   end
 
+  def view
+    @msg = "ありがとう！"
+  end
+
+  def list
+    @books = Book.all
+  end
+
+  def app_var
+    render plain: MY_APP['logo']['source']
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,9 @@ Rails.application.routes.draw do
   # get 'hoge/piyo', to: 'hello/index'
   get 'hello/index'
 
+  get 'hello/view'
+
+  get 'hello/list'
+
+  get ''
 end
